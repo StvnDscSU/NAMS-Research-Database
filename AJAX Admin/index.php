191922dcef8a17cf58d356f801a69a329207aa81
StvnDscSU <stevendscsu@gmail.com>
2021-01-06 16:23:06 -0500

    Initialized Admin Files

--- AJAX Admin/index.php
+++ AJAX Admin/index.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 <head>
-    <title>View Ajax</title>
+    <title>Admin Page</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="style.css">
