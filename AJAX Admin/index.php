1cd5fa25af007b6019e742da8b5608b81f8c9474
StvnDscSU <stevendscsu@gmail.com>
2021-01-06 16:23:16 -0500

    Initialized Admin Files

--- AJAX Admin/index.php
+++ AJAX Admin/index.php
@@ -7,37 +7,76 @@
     <link rel="stylesheet" href="style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
-<div class="container">
-  <!-- SEARCH FUNCTION -->
-  <input type="text" id="search" placeholder="Type to search">
-  <select id="filterList" onchange="filterList()" class='form-control'>
-  <option></option>
-  <option>Mathematics</option>
-  <option>Chemistry</option>
-  <option>Physics</option>
-  </select>
+
+  <div id="button">
+  <!-- BUTTONS -->
+  <button class="button act" onclick="filterButton('professors')">Professors</button> <!-- Buttons whose classes end in "act" will be highlighted. -->
+    <button class="button dorm" onclick="filterButton('research')">Research</button> <!-- Buttons whose classes end in "dorm" will not be highlighted. -->
+    <button class="button dorm" onclick="filterButton('users')">Users</button> <!-- Button classes change upon clicking on a button. -->
+  </div>
   <br><br>
 
-	<table id="professor_table">
-    <thead>
-      <tr class="main_head">
-          <th>Name</th>
-          <th>Discipline</th>
-          <th>Expertise</th>
-      </tr>
-    </thead>
-    <tbody id="table"> <!-- ID must be "table" due to Ajax view script -->
 
-    </tbody>
-  </table>
-</div>
+  <!-- Use the Div Classes to hide and display one table at a time -->
+  <div class="container">
+
+    <div class="filterDiv professors">
+      <table id="professor_table">
+        <thead>
+          <tr class="main_head">
+            <th>Name</th>
+            <th>Email</th>
+            <th>Discipline</th>
+            <th>Expertise</th>
+          </tr>
+        </thead>
+        <tbody id="professor"> <!-- ID must be "table" due to Ajax view script -->
+
+        </tbody>
+      </table>
+    </div>
+
+    <div class="filterDiv research">
+      <table id="research_table">
+        <thead>
+          <tr class="main_head">
+            <th>Name</th>
+            <th>Email</th>
+            <th>Description</th>
+            <th>Compensation</th>
+          </tr>
+        </thead>
+        <tbody id="research"> <!-- ID must be "table" due to Ajax view script -->
+
+        </tbody>
+      </table>
+    </div>
+    <div class="filterDiv users">
+      <table id="user_table">
+        <thead>
+          <tr class="main_head">
+            <th>Username</th>
+            <th>Password</th>
+            <th>Security Access Level</th>
+          </tr>
+        </thead>
+        <tbody id="user"> <!-- ID must be "table" due to Ajax view script -->
+
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+<script>
+</script>
+
 <script>
 	$.ajax({
 		url: "view_ajax.php",
 		type: "POST",
 		cache: false,
 		success: function(data){
-			$('#table').html(data);
+			$('#professor').html(data);
 		}
 	});
 </script>
--- AJAX Admin/view_ajax.php
+++ AJAX Admin/view_ajax.php
@@ -4,8 +4,6 @@ include 'database.php';
 // Query the Professor Table and sets up a query for the subtables.
 $sql = "SELECT * FROM professors";
 $result = $conn->query($sql);
-$query = "SELECT research.name AS Name, research.email AS Email, research.experience AS Experience, Description, Compensation FROM research left join professors on research.email = professors.email";
-$researchvalue = $conn->query($query); // We use this line within a While loop later on.
 
 if ($result->num_rows > 0) {
   $rowID = 0; // Links each row with each subtable by providing both elements with the same ID.
@@ -15,58 +13,11 @@ if ($result->num_rows > 0) {
         <!-- Populates the main table with each professor's Name, Discipline, and Expertise -->
         <tr class="main" id="<?=$rowID;?>"> <!-- Class is main to distinguish styling between Professor rows and subtables -->
             <td><?=$row['Name'];?></td>
+            <td><?=$row['Email'];?></td>
             <td><?=$row['Discipline'];?></td>
             <td><?=$row['Expertise'];?></td>
         </tr>
-
-        <?php
-        // Re-initializes $researchvalue for each professor.
-        $researchvalue = $conn->query($query);
-        /*
-        headerCounter serves two purposes:
-         1. Creates a block of code that only activates once when $headerCounter is 0,
-            this includes initializing the subrow, the subtable, and creating the header for the table.
-         2. If a professor has ongoing research, then a new table is created when creating the header. We need a way to
-            close the table properly. If we end the table after inputing one row of information, then professors with
-            multiple ongoing projects will have multiple tables. We only want one table, so we check to see if
-            $headerCounter was incremented. If so, we close the table and rows properly.
-        */
-        $headerCounter = 0;
-        while ($entry = $researchvalue->fetch_assoc()) { // Iterates through the Research data table
-            if ($entry["Email"] == $row["Email"]) { // If a professor's name appears in both Professor and Research, create a subtable.
-                if ($headerCounter++ == 0) { // Indicates that a subtable was created then increment the headerCounter.
-        ?>
-								<!-- SUBTABALE START -->
-                <!-- Initialize the subtable and create the header -->
-                <tr class="hidden" id="<?=$rowID;?>">
-                <td colspan="3" class="hidden">
-                  <div>
-                    <table class="hidden">
-                        <tr>
-                            <th>Name</th>
-                            <th>Description</th>
-                            <th>Experience</th>
-                            <th>Compensation</th>
-                        </tr>
-                        <?php } // Closes the header and the one-time code block ?>
-                        <!-- Inputs the appropriate information in the subtable. -->
-                        <tr>
-                            <td><?=$entry['Name'];?></td>
-                            <td><?=$entry['Description'];?></td>
-                            <td><?=$entry['Experience'];?></td>
-                            <td><?=$entry['Compensation'];?></td>
-                        </tr>
-            <?php
-            }
-        }
-        if ($headerCounter != 0) { // If the subtable was initialized, then close the subtable.
-        ?>
-                    </table>
-                  </div>
-                </td>
-                </tr>
-                <?php
-        }
+<?php
     }
 }
 else {
