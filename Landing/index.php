4122f44121f354d314918cf187f6eaf5b2cba145
StvnDscSU <stevendscsu@gmail.com>
2021-01-11 15:21:07 -0500

    Added intro, changed font

--- Landing/index.php
+++ Landing/index.php
@@ -37,8 +37,31 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 
 <div class="content">
+  <!-- Introduction -->
+  <!-- Heading Text, explaining the usage of this page to a new student -->
+  <h2 class="page-title">Professor Research</h2>
+  <hr>
+  <p><span style="font-size: 14pt;">
+  Welcome to the Professor Research Database!
+  This page contains a list of professors partnered with the NAMS department
+  to help students find research opportunities on campus.
+  Stockton University offers students a vast selection of undergraduate research
+  opportunities which spans over many fields. Stockton University believes in
+  hands-on learning and encourages research to enrich one's learning process and
+  prepare undergraduates for a post-graduate career. When research is combined
+  with class work, students will fully grasp the concept of their field in terms
+  of knowledge and experience.
+  </span></p>
+  <p><span style="font-size: 14pt;">
+  <b>How does this page work?</b>
+  To use this page, use the search bar below to search for specific terms,
+  disciplines, or professors. When you find a professor with ongoing research,
+  click on their name to view their research opportunities and copy their email.
+  </span></p>
+  <hr>
+
   <!-- LOGIN FUNCTION -->
-  <form action = "" method = "post">
+  <form class="login" action = "" method = "post">
     <input type = "text" name = "username" class = "box" placeholder="Username"/>
     <input type = "password" name = "password" class = "box" placeholder="Password"/>
     <input type = "submit" value = " Administrator Login "/>
@@ -47,10 +70,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
   <!-- SEARCH FUNCTION -->
   <input type="text" id="search" placeholder="Type to search">
   <select id="filterList" onchange="filterList()" class='form-control'>
-  <option></option>
-  <option>Mathematics</option>
-  <option>Chemistry</option>
-  <option>Physics</option>
+    <option></option>
+    <option>Mathematics</option>
+    <option>Chemistry</option>
+    <option>Physics</option>
   </select>
   <br><br>
 
